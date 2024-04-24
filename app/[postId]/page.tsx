502ecc7d466b70d734ad70f5ceb793cf38141f58
kcwww <cwkim0321@gmail.com>
2024-04-24 15:28:42 +0900

    feat: add comments

---
+++ app/[postId]/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata, ResolvingMetadata } from 'next';
+
+import { ORIGIN } from '@/constants/url';
+
+export const generateMetadata = async (
+  {
+    params,
+  }: {
+    params: { postId: string };
+  },
+  parent: ResolvingMetadata
+) => {
+  const id = params.postId;
+  const previousTitle = (await parent).title || '';
+
+  return {
+    title: `Post ${id} | ${previousTitle}`,
+    description: `Post ${id}`,
+    alternate: {
+      canonical: `${ORIGIN}/${id}`,
+    },
+  };
+};
+
+const PostPage = ({ params }: { params: { postId: string } }) => {
+  const id = params.postId;
+  return <main className="">{id}</main>;
+};
+
+export default PostPage;
