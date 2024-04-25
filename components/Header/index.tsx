6b7daea1eec27fc0dde38428aac5fd5bf3135d26
kcwww <cwkim0321@gmail.com>
2024-04-25 15:57:53 +0900

    fix: responsive error

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,27 +9,31 @@ const Header = () => {
   return (
     <header className="w-full flex gap-2">
       <Navbar />
-      <div className="ml-auto flex w-[21rem] gap-4 items-center bg-gray-400 dark:bg-gray-700 p-2 rounded-lg px-4">
-        <Link
-          href="mailto:cwkim0321@gmail.com"
-          className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
-        >
-          <Mail />
-        </Link>
-        <Link
-          href="https://www.instagram.com/kcwww.w/"
-          className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
-        >
-          <Instagram />
-        </Link>
-        <Link
-          href="https://github.com/kcwww"
-          className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
-        >
-          <Github />
-        </Link>
-        <ToggleThemeButton />
-        <CommandBox />
+      <div className="ml-auto flex flex-col sm:flex-row w-fit sm:w-[21rem] gap-4 items-center bg-gray-400 dark:bg-gray-700 p-2 rounded-lg sm:px-4">
+        <div className="flex items-center gap-2">
+          <Link
+            href="mailto:cwkim0321@gmail.com"
+            className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
+          >
+            <Mail />
+          </Link>
+          <Link
+            href="https://www.instagram.com/kcwww.w/"
+            className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
+          >
+            <Instagram />
+          </Link>
+          <Link
+            href="https://github.com/kcwww"
+            className="dark:hover:bg-gray-800 hover:bg-gray-200 p-0 w-8 h-8 flex items-center rounded-md justify-center transition-colors duration-150 ease-out"
+          >
+            <Github />
+          </Link>
+          <ToggleThemeButton />
+        </div>
+        <div className="flex items-center gap-4">
+          <CommandBox />
+        </div>
       </div>
     </header>
   );
