a8580e3ad95df79850a128f6a6de540da971e2b8
kcwww <cwkim0321@gmail.com>
2024-04-26 21:05:21 +0900

    feat: add code only

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import remarkGfm from 'remark-gfm';
 import Image from 'next/image';
-import { ClipboardCopy } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
+import CodeBlock from '@/components/ui/CodeBlock';
 
 const PostContent = ({ markedString }: { markedString: string }) => {
   return (
@@ -18,42 +15,13 @@ const PostContent = ({ markedString }: { markedString: string }) => {
           const match = /language-(\w+)/.exec(className || '');
 
           return !inline && match ? (
-            <div className="relative w-full">
-              <div className="absolute top-2 w-full px-4">
-                <div className="flex justify-between items-center">
-                  <div className="flex gap-2">
-                    <div className="bg-red-700 p-2 rounded-full" />
-                    <div className=" bg-yellow-700 p-2 rounded-full" />
-                    <div className=" bg-green-700 p-2 rounded-full" />
-                  </div>
-
-                  <div className="hidden md:block text-gray-100 top-3">
-                    {match[1]}
-                  </div>
-
-                  <Button className="ml-auto md:ml-0 text-gray-100 top-3 w-7 h-7 p-0 bg-transparent">
-                    <ClipboardCopy size={'1.5rem'} />
-                  </Button>
-                </div>
-              </div>
-
-              <SyntaxHighlighter
-                language={match[1]}
-                {...props}
-                style={oneDark}
-                customStyle={{
-                  padding: '1.5rem',
-                  borderRadius: '10px',
-                  paddingTop: '3rem',
-                  width: '100%',
-                }}
-                component="pre"
-              >
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            </div>
+            <CodeBlock language={match[1]} {...props}>
+              {children}
+            </CodeBlock>
           ) : (
-            <code {...props}>{children}</code>
+            <code className="bg-gray-300 text-red-400 p-1 rounded-md">
+              {children}
+            </code>
           );
         },
         img: (image) => (
--- components/ui/CodeBlock.tsx
+++ components/ui/CodeBlock.tsx
@@ -1,24 +1,51 @@
 import { useEffect } from 'react';
 import hljs from 'highlight.js';
+import { ClipboardCopy } from 'lucide-react';
 import 'highlight.js/styles/atom-one-dark.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-const CodeBlock = ({
-  className,
-  match,
-  children,
-}: {
-  className: string;
-  match: string;
-  children: React.ReactNode;
-}) => {
-  useEffect(() => {
-    hljs.highlightAll();
-  }, []);
+import { Button } from '@/components/ui/button';
 
+export interface CodeBlockProps {
+  language: string;
+  children: string;
+}
+
+const CodeBlock = ({ language, children, ...props }: CodeBlockProps) => {
   return (
-    <pre className={className}>
-      <code className={`language-${match}`}>{children}</code>
-    </pre>
+    <div className="relative w-full">
+      <div className="absolute top-2 w-full px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2">
+            <div className="bg-red-700 p-2 rounded-full" />
+            <div className=" bg-yellow-700 p-2 rounded-full" />
+            <div className=" bg-green-700 p-2 rounded-full" />
+          </div>
+
+          <div className="hidden md:block text-gray-100 top-3">{language}</div>
+
+          <Button className="ml-auto md:ml-0 text-gray-100 top-3 w-7 h-7 p-0 bg-transparent">
+            <ClipboardCopy size={'1.5rem'} />
+          </Button>
+        </div>
+      </div>
+
+      <SyntaxHighlighter
+        language={language}
+        {...props}
+        style={oneDark}
+        customStyle={{
+          padding: '1.5rem',
+          borderRadius: '10px',
+          paddingTop: '3rem',
+          width: '100%',
+        }}
+        component="pre"
+      >
+        {String(children).replace(/\n$/, '')}
+      </SyntaxHighlighter>
+    </div>
   );
 };
 
