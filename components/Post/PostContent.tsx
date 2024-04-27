1e9923d011e2f52e17630e01d684baed4673062c
kcwww <cwkim0321@gmail.com>
2024-04-27 14:36:37 +0900

    fix: build error

--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -12,7 +12,7 @@ const PostContent = ({ markedString }: { markedString: string }) => {
       className="marked-container flex flex-col gap-4 w-full"
       remarkPlugins={[remarkGfm]}
       components={{
-        code({ node, inline, className, children, ...props }) {
+        code({ node, inline, className, children, ...props }: any) {
           const match = /language-(\w+)/.exec(className || '');
 
           return !inline && match ? (
