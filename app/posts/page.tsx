c710afc8bbc77092fa669f0c7c9e3929663b33b0
kcwww <cwkim0321@gmail.com>
2024-04-28 20:04:47 +0900

    chore: add posts introduce

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,16 @@
+import Introduce from '@/components/Main/Introduce';
+
 const PostsPage = () => {
-  return <>Posts</>;
+  const title = 'All Posts';
+  const description = [
+    '포스팅한 모든 게시글을 확인하실 수 있습니다.',
+    '개발 이야기, 일상 이야기 등 다양한 주제로 포스팅하고 있습니다.',
+  ];
+  return (
+    <>
+      <Introduce title={title} description={description} />
+    </>
+  );
 };
 
 export default PostsPage;
