bd9173e5cf5c36cfee1eacbdeba80bf04e7a364d
kcwww <cwkim0321@gmail.com>
2024-04-28 20:08:02 +0900

    chore: add snippets introduce

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -1,5 +1,16 @@
+import Introduce from '@/components/Main/Introduce';
+
 const SnippetsPage = () => {
-  return <div>SnippetsPage</div>;
+  const title = 'All Snippets';
+  const description = [
+    '개발하면서 유용하게 사용할 수 있는 코드 스니펫을 정리하고 있습니다.',
+  ];
+
+  return (
+    <>
+      <Introduce title={title} description={description} />
+    </>
+  );
 };
 
 export default SnippetsPage;
