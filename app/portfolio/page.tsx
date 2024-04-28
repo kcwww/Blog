8ec00ff529f6eb70a79bd1ed3910eff5701e5956
kcwww <cwkim0321@gmail.com>
2024-04-28 20:10:12 +0900

    chore: add portfolio introduce

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,16 @@
+import Introduce from '@/components/Main/Introduce';
+
 const PortfolioPage = () => {
-  return <>portfolio</>;
+  const title = 'Portfolio';
+  const description = [
+    '현재까지 진행한 프로젝트를 소개하는 페이지입니다.',
+    '프로젝트의 상세 내용과 소스코드를 확인하실 수 있습니다.',
+  ];
+  return (
+    <>
+      <Introduce title={title} description={description} />
+    </>
+  );
 };
 
 export default PortfolioPage;
