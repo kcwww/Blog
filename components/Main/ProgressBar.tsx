f94f137e4739d9e99710f9e8e4008df13e6bffea
kcwww <cwkim0321@gmail.com>
2024-05-05 17:15:09 +0900

    feat: add progress bar

---
+++ components/Main/ProgressBar.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+import { Progress } from '@/components/ui/progress';
+
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const updateScrollProgress = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const fullHeight = document.body.scrollHeight;
+
+      const scrolled = (scrollPosition / (fullHeight - windowHeight)) * 100;
+      setProgress(scrolled);
+    };
+
+    window.addEventListener('scroll', updateScrollProgress);
+
+    return () => window.removeEventListener('scroll', updateScrollProgress);
+  }, []);
+
+  return (
+    <>
+      <Progress
+        className="fixed top-0 left-0 w-full rounded-none z-50 h-[0.7rem]"
+        value={progress}
+      />
+    </>
+  );
+};
+
+export default ProgressBar;
--- components/Post/PostContent.tsx
+++ components/Post/PostContent.tsx
@@ -5,48 +5,52 @@ import remarkGfm from 'remark-gfm';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import ProgressBar from '@/components/Main/ProgressBar';
 import CodeBlock from '@/components/ui/CodeBlock';
 import { Button } from '@/components/ui/button';
 
 const PostContent = ({ markedString }: { markedString: string }) => {
   return (
-    <ReactMarkdown
-      className="marked-container flex flex-col w-full mb-20 gap-4"
-      remarkPlugins={[remarkGfm]}
-      components={{
-        code({ node, inline, className, children, ...props }: any) {
-          const match = /language-(\w+)/.exec(className || '');
+    <>
+      <ProgressBar />
+      <ReactMarkdown
+        className="marked-container flex flex-col w-full mb-20 gap-4"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || '');
 
-          return !inline && match ? (
-            <CodeBlock language={match[1]} {...props}>
-              {String(children)}
-            </CodeBlock>
-          ) : (
-            <code className="bg-gray-300 text-red-400 p-1 rounded-md">
-              {children}
-            </code>
-          );
-        },
-        img: (image) => (
-          <Image
-            className="mt-4 rounded-md overflow-hidden ml-auto mr-auto my-4"
-            src={image.src || ''}
-            alt={image.alt || ''}
-            width={560}
-            height={315}
-            objectFit="cover"
-            quality={100}
-          />
-        ),
-        a: (link) => (
-          <Link href={link.href || '#'} target="_blank">
-            <Button>{link.children}</Button>
-          </Link>
-        ),
-      }}
-    >
-      {markedString}
-    </ReactMarkdown>
+            return !inline && match ? (
+              <CodeBlock language={match[1]} {...props}>
+                {String(children)}
+              </CodeBlock>
+            ) : (
+              <code className="bg-gray-300 text-red-400 p-1 rounded-md">
+                {children}
+              </code>
+            );
+          },
+          img: (image) => (
+            <Image
+              className="mt-4 rounded-md overflow-hidden ml-auto mr-auto my-4"
+              src={image.src || ''}
+              alt={image.alt || ''}
+              width={560}
+              height={315}
+              objectFit="cover"
+              quality={100}
+            />
+          ),
+          a: (link) => (
+            <Link href={link.href || '#'} target="_blank">
+              <Button>{link.children}</Button>
+            </Link>
+          ),
+        }}
+      >
+        {markedString}
+      </ReactMarkdown>
+    </>
   );
 };
 export default PostContent;
