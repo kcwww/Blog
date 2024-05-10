aa84850f731f69fcb46bc3017b969e56117aca95
kcwww <cwkim0321@gmail.com>
2024-05-10 20:41:46 +0900

    fix: style error

--- app/(category)/posts/_components/TagDetail.tsx
+++ app/(category)/posts/_components/TagDetail.tsx
@@ -59,18 +59,21 @@ const TagDetail = ({ detail }: { detail: string }) => {
             <AlertTitle>{post.title}</AlertTitle>
             <div className="flex gap-2 sm:flex-row flex-col">
               <AlertDescription className="flex gap-1 justify-center">
-                {post.tags.map((tag) => (
-                  <Badge
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      router.push(ROUTES.TAG(tag));
-                    }}
-                    key={tag}
-                  >
-                    {tag}
-                  </Badge>
-                ))}
+                <div className="grid grid-cols-2 gap-1">
+                  {post.tags.map((tag) => (
+                    <Badge
+                      className="w-full flex justify-center items-center whitespace-nowrap px-4"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        router.push(ROUTES.TAG(tag));
+                      }}
+                      key={tag}
+                    >
+                      {tag}
+                    </Badge>
+                  ))}
+                </div>
               </AlertDescription>
               <p className="text-sm text-gray-400 drak:text-gray-700">
                 {post.createdAt}
