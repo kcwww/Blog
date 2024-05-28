8250f513f22ed6def30a9b289dc7dce584e0a9be
kcwww <cwkim0321@gmail.com>
2024-05-28 15:48:23 +0900

    feat: add snippet metadata

--- app/(category)/snippets/[snippet]/page.tsx
+++ app/(category)/snippets/[snippet]/page.tsx
@@ -1,9 +1,50 @@
-import SnippetDetail from '@/app/(category)/snippets/_components/SnippetDetail';
+import { redirect } from 'next/navigation';
 
-const SnippetDetailPage = ({ params }: { params: { snippet: string } }) => {
+import SnippetDetail from '@/app/(category)/snippets/_components/SnippetDetail';
+import { BACKEND_ROUTES, ROUTES } from '@/constants/routes';
+import serverComponentFetch from '@/lib/fetch/serverComponentFetch';
+import { ReceivedSnippetType } from '@/lib/types/PostType';
+import { ResolvingMetadata } from 'next';
+import { ORIGIN } from '@/constants/url';
+
+const snippetDetailData = async (snippet: string) => {
+  try {
+    const res = await serverComponentFetch(BACKEND_ROUTES.SNIPPETS_ID(snippet));
+    return res.type;
+  } catch (error) {
+    console.error(error);
+    redirect(ROUTES.NOT_FOUND);
+  }
+};
+
+export const generateMetadata = async (
+  { params }: { params: { snippet: string } },
+  parent: ResolvingMetadata
+) => {
+  const data = await snippetDetailData(params.snippet);
+  const previousMetadata = await parent;
+
+  return {
+    ...previousMetadata,
+    title: data.title,
+    openGraph: {
+      ...previousMetadata.openGraph,
+      title: data.title,
+      url: `${ORIGIN}${ROUTES.SNIPPETS}/${params.snippet}`,
+    },
+  };
+};
+
+const SnippetDetailPage = async ({
+  params,
+}: {
+  params: { snippet: string };
+}) => {
+  const data = (await snippetDetailData(params.snippet)) as ReceivedSnippetType;
+
   return (
     <>
-      <SnippetDetail detail={params.snippet} />
+      <SnippetDetail data={data} />
     </>
   );
 };
--- app/(category)/snippets/_components/SnippetDetail.tsx
+++ app/(category)/snippets/_components/SnippetDetail.tsx
@@ -1,45 +1,25 @@
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
 
+import { ROUTES } from '@/constants/routes';
 import { cn } from '@/lib/utils';
-import { BACKEND_ROUTES, ROUTES } from '@/constants/routes';
-import serverComponentFetch from '@/lib/fetch/serverComponentFetch';
-import { ReceivedPostTypeDetail, ReceivedPostType } from '@/lib/types/PostType';
+import { ReceivedSnippetType } from '@/lib/types/PostType';
 import Introduce from '@/components/Main/Introduce';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import BadgeTag from '@/app/(category)/posts/_components/BadgeTag';
 
-type ReceivedSeriesType = ReceivedPostType & {
-  message: string;
-  type: Omit<ReceivedPostTypeDetail, 'description'>;
-};
-
-const snippetDetailData = async (series: string) => {
-  try {
-    const res = await serverComponentFetch(BACKEND_ROUTES.SNIPPETS_ID(series));
-    return res;
-  } catch (error) {
-    console.error(error);
-    redirect(ROUTES.NOT_FOUND);
-  }
-};
-
-const SnippetDetail = async ({ detail }: { detail: string }) => {
-  const data = (await snippetDetailData(detail)) as ReceivedSeriesType;
-
-  const dataDetail = data.type;
-  dataDetail.posts.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
+const SnippetDetail = async ({ data }: { data: ReceivedSnippetType }) => {
+  data.posts.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
 
   return (
     <>
-      <Introduce title={dataDetail?.title || ''} description={null} />
+      <Introduce title={data?.title || ''} description={null} />
       <div className="animate-text-down-delay text-sm text-gray-400 opacity-0 dark:text-gray-500">
-        총 {dataDetail?.posts.length} 개의 포스팅이 존재합니다.
+        총 {data?.posts.length} 개의 포스팅이 존재합니다.
       </div>
-      {dataDetail?.posts.map((post, index) => (
+      {data?.posts.map((post, index) => (
         <Link
           key={post.id}
-          href={ROUTES.TYPE_TO_POST('series', dataDetail.id || '', post.id)}
+          href={ROUTES.TYPE_TO_POST('series', data.id || '', post.id)}
           className="w-full transition-transform hover:scale-105"
         >
           <Alert
