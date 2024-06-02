2567c351dea2d58ddd0f208682ab88f5f4c40f3d
kcwww <cwkim0321@gmail.com>
2024-06-02 17:24:07 +0900

    fix: snippet  page ssr

--- app/(category)/snippets/[snippet]/page.tsx
+++ app/(category)/snippets/[snippet]/page.tsx
@@ -1,16 +1,24 @@
 import { redirect } from 'next/navigation';
+import { doc, getDoc } from 'firebase/firestore';
 
+import { BLOGDB } from '@/lib/Firebase';
 import SnippetDetail from '@/app/(category)/snippets/_components/SnippetDetail';
-import { BACKEND_ROUTES, ROUTES } from '@/constants/routes';
-import serverComponentFetch from '@/lib/fetch/serverComponentFetch';
+import { ROUTES } from '@/constants/routes';
 import { ReceivedSnippetType } from '@/lib/types/PostType';
 import { ResolvingMetadata } from 'next';
 import { ORIGIN } from '@/constants/url';
 
 const snippetDetailData = async (snippet: string) => {
   try {
-    const res = await serverComponentFetch(BACKEND_ROUTES.SNIPPETS_ID(snippet));
-    return res.type;
+    const postRef = doc(BLOGDB, 'snippets', snippet);
+
+    const docSnap = await getDoc(postRef);
+
+    if (docSnap.exists()) {
+      return { id: docSnap.id, ...docSnap.data() } as PostDataType;
+    } else {
+      throw new Error('No Snippets found with ID: ' + id);
+    }
   } catch (error) {
     console.error(error);
     redirect(ROUTES.NOT_FOUND);
@@ -27,11 +35,11 @@ export const generateMetadata = async (
   return {
     ...previousMetadata,
     title: data.title,
-    description : `${data.title} 에 대한 포스팅입니다.`,
+    description: `${data.title} 에 대한 포스팅입니다.`,
     openGraph: {
       ...previousMetadata.openGraph,
       title: data.title,
-      description : `${data.title} 에 대한 포스팅입니다.`,
+      description: `${data.title} 에 대한 포스팅입니다.`,
       url: `${ORIGIN}${ROUTES.SNIPPETS}/${params.snippet}`,
     },
   };
