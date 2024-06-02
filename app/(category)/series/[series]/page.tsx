5a2a1fcbaeceb2b5167cc5298a3969137cb760f3
kcwww <cwkim0321@gmail.com>
2024-06-02 17:19:29 +0900

    fix: series  page ssr

--- app/(category)/series/[series]/page.tsx
+++ app/(category)/series/[series]/page.tsx
@@ -1,16 +1,24 @@
 import { ResolvingMetadata } from 'next';
 import { redirect } from 'next/navigation';
+import { doc, getDoc } from 'firebase/firestore';
 
+import { BLOGDB } from '@/lib/Firebase';
 import SeriesDetail from '@/app/(category)/series/_components/SeriesDetail';
-import serverComponentFetch from '@/lib/fetch/serverComponentFetch';
-import { BACKEND_ROUTES, ROUTES } from '@/constants/routes';
+import { ROUTES } from '@/constants/routes';
 import { ReceivedSeriesType } from '@/lib/types/PostType';
 import { ORIGIN } from '@/constants/url';
 
 const seriesDetailData = async (series: string) => {
   try {
-    const res = await serverComponentFetch(BACKEND_ROUTES.SERIES_ID(series));
-    return res.type;
+    const postRef = doc(BLOGDB, 'series', series);
+
+    const docSnap = await getDoc(postRef);
+
+    if (docSnap.exists()) {
+      return { id: docSnap.id, ...docSnap.data() } as ReceivedSeriesType;
+    } else {
+      throw new Error('No Snippets found with ID: ' + series);
+    }
   } catch (error) {
     console.error(error);
     redirect(ROUTES.NOT_FOUND);
@@ -28,11 +36,11 @@ export const generateMetadata = async (
   return {
     ...previousMetadata,
     title: data.title,
-    description : data.description,
+    description: data.description,
     openGraph: {
       ...previousMetadata.openGraph,
       title: data.title,
-      description : data.description,
+      description: data.description,
       url: `${ORIGIN}${ROUTES.SERIES}/${params.series}`,
     },
   };
