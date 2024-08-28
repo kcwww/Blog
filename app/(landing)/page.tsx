b3daeaaf49c7395ff7f6925b7bc6347f94099c1a
ChanWoo Kim <chanwoki@student.42seoul.kr>
2024-08-28 19:54:48 +0900

    docs: change blog title

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import Introduce from '@/components/Main/Introduce';
 import RecentPosts from '@/app/(landing)/_components/RecentPosts';
 
 const Home = async () => {
-  const title = 'Front-End Developer';
+  const title = 'kcwww dev Blog';
   const description = [
     '안녕하세요, 사람들의 삶에 가치를 더하는 경험을 제공하고자 하는 개발자 김찬우입니다.',
     '사용자 중심의 디자인과 기술 구현을 통해 접근성이 높고, 긍정적인 사용자 경험을 만들어 나가고자 합니다.',
