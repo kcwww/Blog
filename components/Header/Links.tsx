63bbad632596892d08a9dec702cd433bec2f2010
ChanWoo Kim <cwkim0321@gmail.com>
2025-07-11 22:17:29 +0900

    delete: portfolio

--- components/Header/Links.tsx
+++ components/Header/Links.tsx
@@ -64,19 +64,7 @@ const Links = ({ view }: { view: string | undefined }) => {
         )}
       </Link>
 
-      <Link
-        href={'https://bit.ly/3wVaQjS'} // temp
-        className="text-gray-800 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-800 rounded-lg p-2 transition-colors duration-200 ease-out"
-        target="_blank" //temp
-      >
-        {view ? (
-          <SheetTrigger asChild>
-            <p>Portfoilo</p>
-          </SheetTrigger>
-        ) : (
-          'Portfoilo'
-        )}
-      </Link>
+     
     </nav>
   );
 };
